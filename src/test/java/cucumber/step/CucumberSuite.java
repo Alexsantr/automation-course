c0d20c0133eee7381ff4875de72babb1f37f111f
Trek119 <kopen_kin@mail.ru>
2026-04-12 03:42:51 +0300

    update code

---
+++ src/test/java/cucumber/step/CucumberSuite.java
@@ -0,0 +1,15 @@
+package cucumber.step;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.glue", value = "cucumber.step")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty")
+@ConfigurationParameter(key = "cucumber.object-factory", value = "io.cucumber.picocontainer.PicoFactory")
+public class CucumberSuite {
+}
